b0b79e81e4be2c6e820fe360a813eaf632483ede
Hanseul Shim <hanseulshim@gmail.com>
2019-02-15 14:52:27 -0500

    Add font-awesome and header

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,6 @@
   <v-app>
     <v-container grid-list-md fluid>
       <Header />
-      <v-layout row wrap>
-        <v-flex xs12>
-          <v-card dark color="primary">
-            <v-card-text class="px-0">12</v-card-text>
-          </v-card>
-        </v-flex>
-        <v-flex v-for="i in 2" :key="`6${i}`" xs6>
-          <v-card dark color="secondary">
-            <v-card-text class="px-0">6</v-card-text>
-          </v-card>
-        </v-flex>
-        <v-flex v-for="i in 3" :key="`4${i}`" xs4>
-          <v-card dark color="primary">
-            <v-card-text class="px-0">4</v-card-text>
-          </v-card>
-        </v-flex>
-        <v-flex v-for="i in 4" :key="`3${i}`" xs3>
-          <v-card dark color="secondary">
-            <v-card-text class="px-0">3</v-card-text>
-          </v-card>
-        </v-flex>
-        <v-flex v-for="i in 6" :key="`2${i}`" xs2>
-          <v-card dark color="primary">
-            <v-card-text class="px-0">2</v-card-text>
-          </v-card>
-        </v-flex>
-        <v-flex v-for="i in 12" :key="`1${i}`" xs1>
-          <v-card dark color="secondary">
-            <v-card-text class="px-0">1</v-card-text>
-          </v-card>
-        </v-flex>
-      </v-layout>
     </v-container>
     <router-view />
   </v-app>
@@ -57,11 +25,12 @@ body {
   background: #f9f9f9;
   color: #666666;
 }
+
 #app {
   font-weight: 300;
   font-size: 16px;
   line-height: 20px;
-  // padding: 2em 4em;
+  padding: 1em 2em;
   @media (max-width: 1336px) {
     font-size: 13px;
     line-height: 17px;
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="@/assets/logo.png" />
-  </div>
+  <v-layout>
+    <v-flex xs3>
+      <img alt="Vue logo" src="@/assets/logo.png" />
+    </v-flex>
+    <v-flex xs2></v-flex>
+    <v-flex xs2></v-flex>
+    <v-flex class="icon-container-row">
+      <div class="icon-container">
+        <i class="far fa-comment-alt" />
+        <span>Ask Addy</span>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
+  data: function() {
+    return {
+      items: [1, 2, 3, 4, 5]
+    };
+  }
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.icon-container-row {
+  display: flex;
+  justify-content: flex-end;
+}
+.icon-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #4baaa3;
+  cursor: pointer;
+}
+.fa-comment-alt {
+  font-size: 2.5em;
+}
+</style>
