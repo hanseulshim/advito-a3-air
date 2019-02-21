06e5ba7147ff09309f8fff06b23a2886f9c6564d
Hanseul Shim <hanseulshim@gmail.com>
2019-02-20 21:30:00 -0500

    Filter project list if clientId is passed in

--- server/graphql/resolvers/projectResolvers.js
+++ server/graphql/resolvers/projectResolvers.js
@@ -2,5 +2,10 @@ const { fakeInvoke } = require('../helper');
 const { projectList } = require('../../data/projects');
 
 exports.projectResolvers = {
-  projectList: (_, payload) => fakeInvoke(payload, projectList)
+  projectList: (_, payload) => {
+    const projectListFiltered = payload.clientId
+      ? projectList.filter(project => project.clientId === payload.clientId)
+      : projectList;
+    return fakeInvoke(payload, projectListFiltered);
+  }
 };
