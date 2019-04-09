efa0d24c9073d396868f43d74566c4e7b1c6d057
Hanseul Shim <hanseulshim@gmail.com>
2019-04-09 14:10:21 -0400

    Add Contracts setup

---
+++ src/components/Contracts/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="table-spacer">
+    <div class="section-header title-row space-between">
+      <div>
+        <el-tooltip v-if="false" placement="top" effect="light">
+          <div slot="content">
+            You have to have at<br />least one Preferred<br />Airline or this
+            Collection<br />
+            will be deleted in 48<br />hours.
+          </div>
+          <i class="fas fa-exclamation-circle" />
+        </el-tooltip>
+        <span>{{ pluralize('contract', 3) }}</span>
+      </div>
+      <button class="button long">+ NEW CONTRACT</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { pluralize } from '@/helper';
+export default {
+  name: 'Contracts',
+  methods: {
+    pluralize(word, count) {
+      return pluralize(word, count);
+    }
+  }
+};
+</script>
