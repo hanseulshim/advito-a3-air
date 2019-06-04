f4b199f9b12ca6bddfc5cbb8a48476ba32a4ac48
joneil@boostlabs.com <johnoneil@Johns-MacBook-Pro.local>
2019-06-04 17:03:16 -0400

    EOD Tuesday

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div v-if="password === 'air2019'">
-    <Sidebar />
+    <Sidebar/>
     <div id="app">
-      <Header />
-      <router-view />
-      <Modals />
+      <Header/>
+      <router-view/>
+      <Modals/>
     </div>
   </div>
   <div v-else class="validation-container">
@@ -13,32 +13,57 @@
 </template>
 
 <script>
-import Header from '@/components/Header';
-import Sidebar from '@/components/Sidebar';
-import Modals from '@/components/Modals';
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import Modals from "@/components/Modals";
+import { GET_PROJECTS, GET_CLIENT, GET_CLIENTS } from "@/graphql/queries";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     Sidebar,
     Modals
   },
+  apollo: {
+    clientList: {
+      query: GET_CLIENTS
+    },
+    projectList: {
+      query: GET_PROJECTS
+    }
+  },
+  mounted() {
+    console.log(this.$route);
+  },
   data() {
     return {
-      password: 'air2019'
+      password: "air2019",
+      projectList: [],
+      clientList: [],
+      client: null
     };
+  },
+  computed: {
+    // filteredProjectList: function() {
+    //   return this.projectList.filter(
+    //     project =>
+    //       project.effectiveTo === null ||
+    //       this.showInactive ||
+    //       project.effectiveTo > new Date()
+    //   );
+    // }
   }
 };
 </script>
 
 <style lang="scss">
-@import '@/styles/global.scss';
+@import "@/styles/global.scss";
 body {
   margin: 0;
   padding: 0;
   background: $alabaster;
   color: $dove-gray;
-  font-family: 'Rubik', sans-serif;
+  font-family: "Rubik", sans-serif;
   font-weight: 300;
   font-size: 14px;
   line-height: 20px;
@@ -46,7 +71,7 @@ body {
 
 #app {
   padding: 2em 4em;
-  font-family: 'Rubik', sans-serif;
+  font-family: "Rubik", sans-serif;
   max-width: 1600px;
   min-width: 1336px;
   margin: auto;
