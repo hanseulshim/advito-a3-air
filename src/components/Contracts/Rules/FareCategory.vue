5d51d9387519646d0ebfd2f5fe4311a695c4b51a
John O'Neil <joneil@boostlabs.com>
2019-06-26 13:14:08 -0400

    FareCategory

--- src/components/Contracts/Rules/FareCategory.vue
+++ src/components/Contracts/Rules/FareCategory.vue
@@ -1,14 +1,8 @@
 <template>
   <div class="rule-container">
     <p class="rule-title">Fare Category</p>
-    <i
-      v-if="!editMode"
-      class="fas fa-pencil-alt edit-rule"
-      @click="toggleEditMode"
-    />
-    <button v-if="editMode" class="save-rule" @click="toggleEditMode">
-      Save
-    </button>
+    <i v-if="!editMode" class="fas fa-pencil-alt edit-rule" @click="saveRules"/>
+    <button v-if="editMode" class="save-rule" @click="saveRules">Save</button>
     <div v-if="editMode" class="control-row">
       <el-select
         v-model="selectedFares"
@@ -20,78 +14,176 @@
       >
         <el-option
           v-for="fare in faresInDropdown"
-          :key="fare"
-          :label="fare"
-          :value="fare"
+          :key="fare.name"
+          :label="fare.name"
+          :value="fare.cabinId"
         ></el-option>
       </el-select>
+      <label>Exclude:</label>
+      <el-checkbox v-model="exclude" name="exclude"/>
       <button @click="createTag">Add</button>
     </div>
     <div class="rule-tags">
+      <label v-if="includedRules.length">Included:</label>
       <el-tag
-        v-for="rule in rules"
+        v-for="rule in includedRules"
         :key="rule.index"
         type="info"
         size="small"
         closable
         @close="deleteTag(rule)"
-        >{{ rule }}</el-tag
-      >
+      >{{getTagString(rule) }}</el-tag>
+    </div>
+    <div class="rule-tags">
+      <label v-if="excludedRules.length">Excluded:</label>
+      <el-tag
+        v-for="rule in excludedRules"
+        :key="rule.index"
+        type="info"
+        size="small"
+        closable
+        @close="deleteTag(rule)"
+      >{{ getTagString(rule)}}</el-tag>
     </div>
   </div>
 </template>
 <script>
+import { removeTypename } from "@/helper";
+import {
+  GET_FARE_CATEGORY_LIST,
+  GET_FARE_CATEGORY_UNIT_LIST
+} from "@/graphql/queries";
+import { UPDATE_FARE_CATEGORY_LIST } from "@/graphql/mutations";
 export default {
-  name: 'FareCategory',
-  apollo: {},
+  name: "FareCategory",
+  props: {
+    parentId: {
+      default: null,
+      type: Number
+    },
+    tableId: {
+      default: null,
+      type: Number
+    }
+  },
+  apollo: {
+    fareCategoryUnitList: {
+      query: GET_FARE_CATEGORY_UNIT_LIST
+    },
+    fareCategoryList: {
+      query: GET_FARE_CATEGORY_LIST,
+      variables() {
+        return {
+          parentId: this.parentId
+        };
+      },
+      result({ data: { fareCategoryList } }) {
+        return removeTypename(fareCategoryList);
+      }
+    }
+  },
   data() {
     return {
-      fares: [
-        'Business Class',
-        'First Class',
-        'Highly Restricted Economy',
-        'Premium Economy',
-        'Restricted Economy',
-        'Unrestricted Economy'
-      ],
-      editMode: true,
+      fareCategoryUnitList: [],
+      fareCategoryList: [],
       selectedFares: [],
-      rules: []
+      exclude: false,
+      editMode: false
     };
   },
   computed: {
     faresInDropdown() {
-      return this.fares.filter(fare => !this.rules.includes(fare));
+      return this.fareCategoryUnitList.filter(
+        fare => !this.fareCategoryList.some(v => v.fareCategory === fare.id)
+      );
+    },
+    excludedRules() {
+      return this.fareCategoryList.filter(rule => rule.exclude);
+    },
+    includedRules() {
+      return this.fareCategoryList.filter(rule => !rule.exclude);
     }
   },
   methods: {
-    toggleEditMode() {
-      if (this.editMode && !this.rules.length) {
-        this.$emit('delete-rule', 'FareCategory');
+    async saveRules() {
+      if (this.editMode && !this.fareCategoryList.length) {
+        this.$emit("delete-rule", "FareCategory");
+      } else if (this.editMode) {
+        await this.$apollo.mutate({
+          mutation: UPDATE_FARE_CATEGORY_LIST,
+          variables: {
+            parentId: this.parentId,
+            fareCategoryList: this.fareCategoryList
+          },
+          refetchQueries: () => [
+            {
+              query: GET_FARE_CATEGORY_LIST,
+              variables: { parentId: this.parentId }
+            }
+          ]
+        });
       }
       this.editMode = !this.editMode;
+      this.selectedFares = [];
     },
     createTag() {
-      this.selectedFares.map(v => {
-        const repeat = this.rules.includes(v);
-        if (repeat) {
-          return;
-        } else {
-          this.rules.push(v);
-        }
-      });
+      const ruleContainerId = this.fareCategoryList.length
+        ? this.fareCategoryList[0].ruleContainerId
+        : null;
+
+      this.selectedFares.map(v =>
+        this.fareCategoryList.push({
+          id: null,
+          ruleContainerId,
+          fareCategory: v,
+          exclude: this.exclude,
+          isDeleted: false
+        })
+      );
 
       this.selectedFares = [];
     },
-    deleteTag(tag) {
-      this.rules.splice(this.rules.indexOf(tag), 1);
-      if (!this.rules.length) {
-        this.$emit('delete-rule', 'FareCategory');
+    async deleteTag(tag) {
+      const idx = this.fareCategoryList.indexOf(tag);
+      this.fareCategoryList[idx].isDeleted = true;
+
+      await this.$apollo
+        .mutate({
+          mutation: UPDATE_FARE_CATEGORY_LIST,
+          variables: {
+            parentId: this.parentId,
+            fareCategoryList: this.fareCategoryList
+          },
+          refetchQueries: () => [
+            {
+              query: GET_FARE_CATEGORY_LIST,
+              variables: { parentId: this.parentId }
+            }
+          ]
+        })
+        .then(() => {
+          const rulesRemaining = this.fareCategoryList.some(
+            rule => !rule.isDeleted
+          );
+          if (!this.fareCategoryList.length || !rulesRemaining) {
+            this.$emit("delete-rule", "FareCategory");
+          }
+        });
+    },
+    getTagString(rule) {
+      if (!this.fareCategoryList.length || !this.fareCategoryUnitList.length) {
+        return;
+      } else {
+        const unit = this.fareCategoryUnitList.filter(
+          unit => unit.id === rule.fareCategory
+        )[0];
+
+        return `${unit.name}`;
       }
     }
   }
 };
 </script>
 <style lang="scss">
-@import './ruleStyles.scss';
+@import "./ruleStyles.scss";
 </style>
