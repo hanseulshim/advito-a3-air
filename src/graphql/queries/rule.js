3adf28de1ead8b6732fea4631078df3c96b537ed
joneil@boostlabs.com <johnoneil@Johns-MacBook-Pro.local>
2019-06-27 16:06:00 -0400

    geo list is back

--- src/graphql/queries/rule.js
+++ src/graphql/queries/rule.js
@@ -6,6 +6,16 @@ export const GET_RULE_LIST = gql`
   }
 `;
 
+export const GET_GEO_LIST = gql`
+  query ruleList {
+    ruleList {
+      code
+      name
+      locationType
+    }
+  }
+`;
+
 export const GET_TICKETING_DATE_LIST = gql`
   query ticketingDateList($parentId: Int, $parentType: Int) {
     ticketingDateList(parentId: $parentId, parentType: $parentType) {
