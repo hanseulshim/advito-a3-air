5cbfc66fc9f42bff4454791a976bb2a8e0c104a1
joneil@boostlabs.com <johnoneil@Johns-MacBook-Pro.local>
2019-07-12 14:13:00 -0400

    refetch ticketing dates

--- src/components/Contracts/PricingTerms/Discounts/PricingTermRulesModal.vue
+++ src/components/Contracts/PricingTerms/Discounts/PricingTermRulesModal.vue
@@ -37,7 +37,9 @@
         :key="rule.id"
         :parent-id="discount.id"
         :table-id="rule.id"
+        :pricing-term-id="discount.pricingTermId"
         :parent-type="parentType"
+        :contract-id="selectedContract.id"
         @delete-rule="deleteRule"
       ></component>
     </div>
@@ -72,7 +74,11 @@ import Cabin from '../../Rules/Cabin';
 import FareCategory from '../../Rules/FareCategory';
 
 import { ruleTypes } from '../../Rules/helper';
-import { GET_RULE_LIST, GET_DISCOUNT } from '@/graphql/queries';
+import {
+  GET_RULE_LIST,
+  GET_DISCOUNT,
+  GET_SELECTED_CONTRACT
+} from '@/graphql/queries';
 export default {
   name: 'PricingTermRulesModal',
   apollo: {
@@ -91,6 +97,9 @@ export default {
           );
         }
       }
+    },
+    selectedContract: {
+      query: GET_SELECTED_CONTRACT
     }
   },
   components: {
@@ -127,7 +136,8 @@ export default {
       ruleList: [],
       renderedRules: [],
       selectedRule: '',
-      parentType: 1
+      parentType: 1,
+      selectedContract: null
     };
   },
   computed: {
