23acca4ed47a5c92b78d8ec53e8929d8c5da34ef
joneil@boostlabs.com <johnoneil@Johns-MacBook-Pro.local>
2019-07-19 14:09:15 -0400

    validation

--- src/components/Contracts/BulkActionRules/TicketDate.vue
+++ src/components/Contracts/BulkActionRules/TicketDate.vue
@@ -165,12 +165,18 @@ export default {
       this.updateRule = null;
     },
     createTag() {
-      this.ticketingDateList.push({
-        startDate: new Date(this.startDate),
-        endDate: new Date(this.endDate)
-      });
-      this.startDate = '';
-      this.endDate = '';
+      if (this.startDate && this.endDate) {
+        this.ticketingDateList.push({
+          startDate: new Date(this.startDate),
+          endDate: new Date(this.endDate)
+        });
+        this.startDate = '';
+        this.endDate = '';
+      } else {
+        this.$modal.show('error', {
+          message: 'Please enter both a start date and an end date.'
+        });
+      }
     },
     deleteTag(tag) {
       const idx = this.ticketingDateList.indexOf(tag);
