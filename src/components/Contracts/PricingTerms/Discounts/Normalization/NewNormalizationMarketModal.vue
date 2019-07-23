e655a965d3867624963efe094ba254321481b10b
joneil@boostlabs.com <johnoneil@Johns-MacBook-Pro.local>
2019-07-23 14:17:50 -0400

    norm market modal

--- src/components/Contracts/PricingTerms/Discounts/Normalization/NewNormalizationMarketModal.vue
+++ src/components/Contracts/PricingTerms/Discounts/Normalization/NewNormalizationMarketModal.vue
@@ -3,10 +3,14 @@
     classes="modal-container"
     name="new-normalization-market-modal"
     height="auto"
-    width="1000px"
+    width="1200px"
     @before-open="beforeOpen"
     @before-close="beforeClose"
   >
+    <div class="title-row space-between">
+      <div class="section-header">new normalization market</div>
+      <i class="fas fa-times close-modal-button" @click="hideModal"></i>
+    </div>
     <div class="newMarketContainer">
       <el-form
         ref="newMarket"
@@ -15,22 +19,50 @@
         label-position="top"
         label-width="150px"
         hide-required-asterisk
-        :inline="true"
         class="newMarketForm"
       >
-        <div class="title-row space-between">
-          <div class="section-header">new normalization market</div>
-          <i class="fas fa-times close-modal-button" @click="hideModal"></i>
+        <div class="flex-row space-between">
+          <el-form-item label="Select New Market *" class="flex2">
+            <el-select placeholder="Select Top Market" />
+          </el-form-item>
+          <el-form-item label="Override Usage" class="flex1">
+            <el-input-number />
+          </el-form-item>
+          <el-form-item label="Travel Date *" class="flex1">
+            <el-date-picker />
+          </el-form-item>
         </div>
-        <el-form-item label="Select New Market *">
-          <el-select placeholder="Select Top Market" />
-        </el-form-item>
-        <el-form-item label="Override Usage">
-          <el-input-number />
-        </el-form-item>
-        <el-form-item label="Travel Date *">
-          <el-date-picker />
-        </el-form-item>
+        <div class="flex-row">
+          <el-form-item label="Notes" class="flex1">
+            <el-input
+              v-model="text"
+              v-loading="$apollo.loading"
+              class="market-notes"
+              placeholder="Enter note here"
+              type="textarea"
+            />
+          </el-form-item>
+        </div>
+        <div class="flex-row">
+          <el-form-item label="Fare Basis" class="flex1">
+            <el-input />
+          </el-form-item>
+          <el-form-item label="RT/OW" class="flex1">
+            <el-input />
+          </el-form-item>
+          <el-form-item label="Amount" class="flex1">
+            <el-input />
+          </el-form-item>
+          <el-form-item label="Currency" class="flex1">
+            <el-input />
+          </el-form-item>
+          <el-form-item label="AP (Days)" class="flex1">
+            <el-input />
+          </el-form-item>
+          <el-form-item label="Min Stay (Days)" class="flex1">
+            <el-input />
+          </el-form-item>
+        </div>
         <el-form-item class="save-container">
           <button class="button" type="button" @click="validateForm">
             SAVE
@@ -38,7 +70,53 @@
         </el-form-item>
       </el-form>
       <div class="newMarketTables">
-        <p>Im the table</p>
+        <div class="flex-row">
+          <el-table
+            :data="tableData"
+            stripe
+            :cell-style="{ padding: '0', height: '20px' }"
+          >
+            <el-table-column prop="label" label="Adv. Tkt. (Days)" width="auto">
+            </el-table-column>
+            <el-table-column prop="percent" width="75">
+              <template slot-scope="props">
+                {{ props.row.percent }}%
+              </template>
+            </el-table-column>
+          </el-table>
+          <el-table
+            :data="tableData2"
+            stripe
+            :cell-style="{ padding: '0', height: '20px' }"
+            size="small"
+          >
+            <el-table-column prop="label" label="Departure" width="100">
+            </el-table-column>
+            <el-table-column prop="percent" width="100">
+              <template slot-scope="props">
+                {{ props.row.percent }}%
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+        <div class="flex-row">
+          <el-table
+            :data="tableData3"
+            stripe
+            :cell-style="{ padding: '0', height: '20px' }"
+            style="margin-top: 20px"
+          >
+            <el-table-column prop="fareBasis" label="Fare Basis">
+            </el-table-column>
+            <el-table-column prop="bookingClass" label="Booking Class">
+            </el-table-column>
+            <el-table-column prop="usage" label="Usage"
+              ><template slot-scope="props">
+                {{ props.row.usage }}%
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
       </div>
     </div>
   </modal>
@@ -49,7 +127,73 @@ export default {
   apollo: {},
   data() {
     return {
-      form: {},
+      form: {
+        text: ''
+      },
+      tableData: [
+        {
+          label: '0-2',
+          percent: 5
+        },
+        {
+          label: '3-4',
+          percent: 20
+        },
+        {
+          label: '7-10',
+          percent: 11
+        },
+        {
+          label: '14-20',
+          percent: 25
+        },
+        {
+          label: '21+',
+          percent: 10
+        }
+      ],
+      tableData2: [
+        {
+          label: 'Sunday',
+          percent: 5
+        },
+        {
+          label: 'Monday',
+          percent: 20
+        },
+        {
+          label: 'Tuesday',
+          percent: 11
+        },
+        {
+          label: 'Wednesday',
+          percent: 25
+        },
+        {
+          label: 'Thursday',
+          percent: 10
+        },
+        {
+          label: 'Friday',
+          percent: 10
+        },
+        {
+          label: 'Saturday',
+          percent: 10
+        }
+      ],
+      tableData3: [
+        {
+          fareBasis: 'DGFBLM',
+          bookingClass: 'D',
+          usage: 98
+        },
+        {
+          fareBasis: 'J1NQO4C5',
+          bookingClass: 'D',
+          usage: 2
+        }
+      ],
       rules: {}
     };
   },
@@ -65,6 +209,9 @@ export default {
           return false;
         }
       });
+    },
+    rowClassName() {
+      return 'rowStyle';
     },
     async newMarket() {
       //   try {
@@ -144,10 +291,46 @@ export default {
 
   .newMarketForm {
     flex: 2;
+    margin-right: 2em;
+    .el-form-item {
+      margin-right: 1em;
+    }
+  }
+
+  .flex-row {
+    display: flex;
+
+    .el-table {
+      margin-right: 1em;
+    }
+  }
+
+  .flex1 {
+    flex: 1;
+  }
+
+  .flex2 {
+    flex: 2;
+  }
+
+  .flex3 {
+    flex: 3;
+  }
+
+  .space-between {
+    justify-content: space-between;
   }
 
   .newMarkettables {
     flex: 1;
+  }
+
+  .inline {
+    display: inline;
+  }
+
+  .rowStyle {
+    height: 15px;
   }
 }
 </style>
