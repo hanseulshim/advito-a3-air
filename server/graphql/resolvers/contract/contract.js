dd82b5ba5fc6ea33d9895086154749778e3bc08d
Hanseul Shim <hanseulshim@gmail.com>
2019-08-20 10:10:24 -0400

    Update contract container copy function in db

--- server/graphql/resolvers/contract/contract.js
+++ server/graphql/resolvers/contract/contract.js
@@ -56,7 +56,7 @@ exports.contract = {
     },
     copyContract: async (_, { id, name }, { db }) => {
       const { rows } = await db.raw(
-        `SELECT contract_createcopy(${id}, '${name}', null, null)`
+        `SELECT contractcontainer_createcopy(${id}, '${name}')`
       );
       const [copyContract] = rows;
       const [contract] = await getContract(
