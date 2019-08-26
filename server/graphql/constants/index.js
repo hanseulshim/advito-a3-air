0742db49ace8c73eac9c3cf94db0930fe5ea3d7d
Hanseul Shim <hanseulshim@gmail.com>
2019-08-26 16:48:45 -0400

    Update email URL

--- server/graphql/constants/index.js
+++ server/graphql/constants/index.js
@@ -31,7 +31,9 @@ const {
 } = require('./collection');
 const { ADVITO_AIR_APPLICATION, EMAIL_SENDER, EMAIL_BCC } = require('./email');
 
-const APP_URL = 'http://localhost:8080/#';
+// const APP_URL = 'http://localhost:8080/#';
+const APP_URL =
+  'https://s3.amazonaws.com/alpha.boostlabs/air-dev/index.html?#/';
 
 module.exports = {
   AIR_APPLICATION_ID,
