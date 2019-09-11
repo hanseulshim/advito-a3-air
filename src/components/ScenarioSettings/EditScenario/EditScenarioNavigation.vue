f39badf61b7c2fef009c2a26f61012618e402398
joneil@boostlabs.com <joneil@boostlabs.com>
2019-09-11 09:37:59 -0500

    nav styling sorted

--- src/components/ScenarioSettings/EditScenario/EditScenarioNavigation.vue
+++ src/components/ScenarioSettings/EditScenario/EditScenarioNavigation.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="scenario-navigation-container">
       <router-link
-        class="data-nav-item nav-item data-nav"
+        class="scenario-nav-item nav-item"
         :to="
           `/${
             $route.params.projectId
@@ -17,9 +17,10 @@
         }"
       >
         <span class="nav-title">Scenario Name</span>
+        <i class="fas fa-chevron-up" />
       </router-link>
       <router-link
-        class="data-nav-item nav-item data-nav"
+        class="scenario-nav-item nav-item"
         :to="
           `/${
             $route.params.projectId
@@ -34,9 +35,10 @@
         }"
       >
         <span class="nav-title">Contracts</span>
+        <i class="fas fa-chevron-up" />
       </router-link>
       <router-link
-        class="data-nav-item nav-item data-nav"
+        class="scenario-nav-item nav-item"
         :to="
           `/${
             $route.params.projectId
@@ -51,9 +53,10 @@
         }"
       >
         <span class="nav-title"> Preferred Airlines</span>
+        <i class="fas fa-chevron-up" />
       </router-link>
       <router-link
-        class="data-nav-item nav-item data-nav"
+        class="scenario-nav-item nav-item"
         :to="
           `/${
             $route.params.projectId
@@ -68,9 +71,10 @@
         }"
       >
         <span class="nav-title">Parameters</span>
+        <i class="fas fa-chevron-up" />
       </router-link>
       <router-link
-        class="data-nav-item nav-item data-nav"
+        class="scenario-nav-item nav-item"
         :to="
           `/${
             $route.params.projectId
@@ -85,56 +89,13 @@
         }"
       >
         <span class="nav-title">Trip Distribution</span>
+        <i class="fas fa-chevron-up" />
       </router-link>
     </div>
-    <!-- <div class="sub-navigation-container">
-      <ScenarioName
-        v-if="
-          $route.path.includes(
-            `/${$route.params.projectId}/scenario-settings/scenario-name`
-          )
-        "
-      />
-      <ScenarioContracts
-        v-if="
-          $route.path.includes(
-            `/${$route.params.projectId}/scenario-settings/scenario-contracts`
-          )
-        "
-      />
-      <ScenarioPreferredAirlines
-        v-if="
-          $route.path.includes(
-            `/${
-              $route.params.projectId
-            }/scenario-settings/scenario-preferred-airlines`
-          )
-        "
-      />
-      <ScenarioParamters
-        v-if="
-          $route.path.includes(
-            `/${$route.params.projectId}/scenario-settings/scenario-parameters`
-          )
-        "
-      />
-      <ScenarioTripDistribution
-        v-if="
-          $route.path.includes(
-            `/${
-              $route.params.projectId
-            }/scenario-settings/scenario-trip-distribution`
-          )
-        "
-      />
-    </div> -->
   </div>
 </template>
 
 <script>
-// import PosTrends from './PosTrends';
-// import DivisionTrends from './DivisionTrends';
-// import ImportErrors from './ImportErrors';
 export default {
   name: 'EditScenarioNavigation',
   components: {},
@@ -147,71 +108,34 @@ export default {
 .scenario-navigation-container {
   display: flex;
   align-items: center;
+  margin-top: 2em;
+  border-bottom: 2px solid $botticelli;
+  padding-bottom: 10px;
 }
 
-.dataset {
-  margin-left: auto;
-}
-.data-nav-item {
-  &.data-nav {
-    display: flex;
-    align-items: center;
-    margin-right: 6em;
-  }
+.scenario-nav-item {
+  margin-right: 6em;
+  position: relative;
   .nav-title {
-    font-size: 16px;
+    font-size: 14px;
   }
   &.active {
     span {
       color: $tradewind;
     }
-    .data-icon {
-      border-color: $tradewind;
+    .fa-chevron-up {
+      visibility: visible;
     }
   }
   &:hover {
     span {
       color: $tradewind;
     }
-    .data-icon {
-      border-color: $tradewind;
-    }
-  }
-  .data-icon {
-    border: 1px solid $dusty-gray;
-    border-radius: 100%;
-    padding: 10px;
-    margin-right: 10px;
-    width: 35px;
-    height: 35px;
-    img {
-      width: 100%;
-    }
-  }
-}
-.sub-navigation-container {
-  border-top: 2px solid $botticelli;
-  margin-top: 10px;
-  padding-top: 10px;
-  position: relative;
-  .pos-trends {
-    margin-left: 15%;
-    .fa-chevron-up {
-      left: calc(15% + 1em + 22.375px);
-    }
-  }
-  .division-trends {
-    margin-left: 30%;
-    .fa-chevron-up {
-      left: calc(30% + 2em + 22.375px);
-    }
-  }
-  .spacer {
-    margin: 0 15px;
   }
   .fa-chevron-up {
     position: absolute;
-    top: -10px;
+    visibility: hidden;
+    bottom: -15px;
     background: $white;
     color: $botticelli;
     left: 22.375px;
