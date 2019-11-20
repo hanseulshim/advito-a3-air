4902342d15980098f3aa5c5555f7cb8e493d5a70
Hanseul Shim <hanseulshim@gmail.com>
2019-11-20 10:09:13 -0500

    Update farebasis

--- server/resolvers/contract/rule.js
+++ server/resolvers/contract/rule.js
@@ -563,7 +563,7 @@ const getRuleInfo = id => {
         },
         {
           name: 'matchExclude',
-          type: 'string'
+          type: 'boolean'
         },
         {
           name: 'matchValue',
